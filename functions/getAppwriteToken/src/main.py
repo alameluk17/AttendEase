660fe250fc17f64221e7f98e54d8b1f069693616
Aathish Sivasubrahmanian <aathish04@gmail.com>
2024-10-25 15:27:47 +0530

    Start actually developing the function.

--- functions/getAppwriteToken/src/main.py
+++ functions/getAppwriteToken/src/main.py
@@ -7,24 +7,22 @@ load_dotenv()
 
 # This Appwrite function will be executed every time your function is triggered
 def main(context):
-    # You can use the Appwrite SDK to interact with other services
-    # For this example, we're using the Users service
-    print(context)
-    print(context.req)
-    print(context.req.headers)
-    client = (
-        Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
-    )
-    users = Users(client)
-    token = users.create_token('671b4700002a7f94b1da')
-    secret = token['secret']
-
-    return context.res.json(
-        {
-            "token" : secret,
-            "context":context.req.body
-        }
-    )
+    responsebody = {
+        "error":None,
+        "token":None,
+    }
+    try:
+        client = (
+            Client()
+            .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
+            .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
+            .set_key(context.req.headers["x-appwrite-key"])
+        )
+        users = Users(client)
+        token = users.create_token('671b4700002a7f94b1da')
+        secret = token['secret']
+        responsebody["token"] = secret
+        return context.res.json(responsebody)
+    except Exception as e:
+        responsebody["error"] = e
+        return context.res.json(responsebody,500)
