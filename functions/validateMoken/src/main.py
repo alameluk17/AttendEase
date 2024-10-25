e86671fdebca7fc5a6aa93bf40e376a9b7702ebc
alameluk17 <alamukanna14@gmail.com>
2024-10-26 04:03:44 +0530

    appwrite users test

--- functions/validateMoken/src/main.py
+++ functions/validateMoken/src/main.py
@@ -1,5 +1,6 @@
 from appwrite.client import Client
 from appwrite.services.users import Users
+from appwrite.query import Query
 from appwrite.exception import AppwriteException
 import os
 from pymoodle import MoodleWebServiceAPIClient, MoodleError,MoodleException
@@ -17,13 +18,21 @@ def main(context):
             .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
             .set_key(context.req.headers["x-appwrite-key"])
         )
+        users = Users(client)
         jsonobj = context.req.body_json
         if "moken" not in jsonobj:
             raise Exception("`moken` must be provided in the JSON Request Body.")
         moodle = MoodleWebServiceAPIClient(token=jsonobj["moken"],api_base="https://lms.ssn.edu.in")
         moodleUserId = moodle.CLIENT_USER_DATA["userid"]
         user_data = moodle.get_users_by_field("id",[moodleUserId])
-        print(user_data)
+        emailAddress= user_data[0]["email"]
+        rollNo = user_data["idnumber"]
+        
+        result = users.list(Query.equal("email",emailAddress))
+        print(result)
+        # if not(result):
+        #     #new user
+
         moodle.get_user_courses(userid=moodle.CLIENT_USER_DATA["userid"])       
         return context.res.json(user_data)
     except Exception as e:
