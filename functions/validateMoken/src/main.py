e8422a4afd6ea7bc371d345cabd09d1c5971cf45
Aathish Sivasubrahmanian <aathish04@gmail.com>
2024-10-25 21:27:02 +0530

    Use my own moodleapi client

--- functions/validateMoken/src/main.py
+++ functions/validateMoken/src/main.py
@@ -1,33 +1,31 @@
 from appwrite.client import Client
 from appwrite.services.users import Users
 from appwrite.exception import AppwriteException
+
 import os
-from moodle import Moodle
+from moodleapi import MoodleWebServiceAPIClient
 from dotenv import load_dotenv
 load_dotenv()
-from dataclasses import dataclass, asdict
 import requests
 
 # This Appwrite function will be executed every time your function is triggered
 def main(context):
     # You can use the Appwrite SDK to interact with other services
     # For this example, we're using the Users service
-    client = (
-        Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
-    )
-    users = Users(client)
-    print(context.req.body)
     try:
+        client = (
+            Client()
+            .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
+            .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
+            .set_key(context.req.headers["x-appwrite-key"])
+        )
+        print(context.req.body)
         jsonobj = context.req.body_json
         if "moken" not in jsonobj:
             raise Exception("`moken` must be provided in the JSON Request Body.")
-        moodle = Moodle("https://lms.ssn.edu.in/webservice/rest/server.php", jsonobj["moken"])
-        site_info = moodle.core.webservice.get_site_info()
-        site_info = moodle('core_webservice_get_site_info')
-        return context.res.json({"info":site_info})
+        moodle = MoodleWebServiceAPIClient(token=jsonobj["moken"],api_base="https://lms.ssn.edu.in")
+
+        return context.res.json({"info":moodle.CLIENT_USER_DATA})
     except Exception as e:
         print(e)
         return context.res.json(
