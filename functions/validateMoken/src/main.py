4fc825d6d8226f5fe559ec2056b813953468ce57
Aathish Sivasubrahmanian <aathish04@gmail.com>
2024-10-26 18:06:31 +0530

    test uuid

--- functions/validateMoken/src/main.py
+++ functions/validateMoken/src/main.py
@@ -1,12 +1,16 @@
 from appwrite.client import Client
+from appwrite.services.databases import Databases
 from appwrite.services.users import Users
 from appwrite.query import Query
 from appwrite.id import ID
 from appwrite.exception import AppwriteException
 import os
+from uuid import uuid5, NAMESPACE_URL
 from pymoodle import MoodleWebServiceAPIClient, MoodleError,MoodleException
-import requests
 
+
+def genUUID(email):
+    return str(uuid5(NAMESPACE_URL, email))
 
 # This Appwrite function will be executed every time your function is triggered
 def main(context):
@@ -20,28 +24,32 @@ def main(context):
             .set_key(context.req.headers["x-appwrite-key"])
         )
         users = Users(client)
-        jsonobj = context.req.body_json
-        if "moken" not in jsonobj:
-            raise Exception("`moken` must be provided in the JSON Request Body.")
-        moodle = MoodleWebServiceAPIClient(token=jsonobj["moken"],api_base="https://lms.ssn.edu.in")
+        databases = Databases(client)
+
+        if "moken" not in context.req.body_json:
+            raise ValueError("`moken` must be provided in the JSON Request Body.")
+
+        moodle = MoodleWebServiceAPIClient(token=context.req.body_json["moken"],api_base="https://lms.ssn.edu.in")
         moodleUserId = moodle.site_info["userid"]
         user_data = moodle.core_user_get_users_by_field("id",[moodleUserId])
+
         emailAddress= user_data[0]["email"]
-        rollNo = user_data[0]["idnumber"]
+        rollnum = user_data[0]["idnumber"]
         name = user_data[0]["fullname"]
-        
-        result = users.list([Query.equal("email",emailAddress)])
-        print(result)
-        if result["total"] < 1:
-            # Create New User
-            result = users.create(
-            user_id = ID.unique(),
-            email = emailAddress, # optional
-            name = name # optional
-            )
-            print(result)
-          
-        return context.res.json(moodle.core_enrol_get_users_courses(userid=moodle.site_info["userid"]))
+
+        authusers = users.get(genUUID(emailAddress))
+        # if authusers["total"] < 1:
+        #     userid = ID.unique(),
+        #     result = users.create(
+        #         user_id = userid,
+        #         email = emailAddress,
+        #         name = name
+        #     )
+        #     databases.create_document("attendease","users",userid,{"email":emailAddress,"rollnum":rollnum})
+        # authuser = users.list([Query.equal("email",emailAddress)])[0]
+        # datauser = databases.get_document("attendease","users",authuser.)
+
+        return context.res.json({"user":"hi"})
     except Exception as e:
         print(e)
         return context.res.json(
