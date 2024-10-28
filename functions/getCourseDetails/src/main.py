99cc88eb7abf96251fe4350d3f71041418c1b95e
Appwrite <team@appwrite.io>
2024-10-28 11:27:08 +0000

    Create 'getCourseDetails' function

--- functions/getCourseDetails/src/main.py
+++ functions/getCourseDetails/src/main.py
@@ -1,7 +1,5 @@
 from appwrite.client import Client
 from appwrite.services.users import Users
-from appwrite.services.databases import Databases
-from pymoodle import MoodleWebServiceAPIClient, MoodleError,MoodleException
 from appwrite.exception import AppwriteException
 import os
 
@@ -9,32 +7,33 @@ import os
 def main(context):
     # You can use the Appwrite SDK to interact with other services
     # For this example, we're using the Users service
+    client = (
+        Client()
+        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
+        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
+        .set_key(context.req.headers["x-appwrite-key"])
+    )
+    users = Users(client)
+
     try:
-        client = (
-            Client()
-            .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-            .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-            .set_key(context.req.headers["x-appwrite-key"])
-        )
-        users = Users(client)
+        response = users.list()
+        # Log messages and errors to the Appwrite Console
+        # These logs won't be seen by your end users
+        context.log("Total users: " + str(response["total"]))
+    except AppwriteException as err:
+        context.error("Could not list users: " + repr(err))
 
-        if "moken" not in context.req.body_json:
-            raise ValueError("`moken` must be provided in the JSON Request Body.")
-        
-        if "courseId" not in context.req.body_json:
-            raise ValueError("`courseId` must be provided in the JSON Request Body.")
+    # The req object contains the request data
+    if context.req.path == "/ping":
+        # Use res object to respond with text(), json(), or binary()
+        # Don't forget to return a response!
+        return context.res.text("Pong")
 
-        moodle = MoodleWebServiceAPIClient(token=context.req.body_json["moken"],api_base="https://lms.ssn.edu.in")
-        moodleUserId = moodle.site_info["userid"]
-        print(moodleUserId)
-        courseDetails = moodle.core_enrol_get_enrolled_users(courseid=context.req.body_json["courseId"])
-        
-        # print(courses)
-        return context.res.json(
-            {"CourseDetails" : courseDetails}
-        )
-    except Exception as e:
-        print(e)
-        return context.res.json(
-            {"Error":str(e)},500
-        )
+    return context.res.json(
+        {
+            "motto": "Build like a team of hundreds_",
+            "learn": "https://appwrite.io/docs",
+            "connect": "https://appwrite.io/discord",
+            "getInspired": "https://builtwith.appwrite.io",
+        }
+    )
